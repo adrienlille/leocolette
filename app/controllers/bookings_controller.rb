61c42c30f2a80173828bce5acfe0257aea7093a8
Adrien du Peloux <adrien.dupeloux@edhec.com>
2018-06-13 15:00:19 +0200

    any user can book

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,14 @@
 class BookingsController < ApplicationController
+  def create
+    authorize @booking = Booking.new(safe_params)
+    @booking.save!
+    apartment = Apartment.find(params[:apartment_id])
+    redirect_to apartment_path(apartment)
+  end
 
+  private
+
+  def safe_params
+    params.require(:booking).permit(:apartment, :start_date, :end_date)
+  end
 end
