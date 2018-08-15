021e47d150baeb4e57b642922acea057790d9126
Adrien du Peloux <adrien.dupeloux@yahoo.fr>
2018-08-15 12:58:28 +0200

    registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  devise_for :users, :controllers => { registrations: 'registrations' }
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :apartments do
     collection do                       # collection => no restaurant id in URL
         get 'mine', to: "apartments#mine"  # RestaurantsController#mine
