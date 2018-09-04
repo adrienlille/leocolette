b0794758b493b53764e44d3bf78ef2301202ff55
Adrien du Peloux <adrien.dupeloux@yahoo.fr>
2018-09-04 15:28:00 +0200

    fixed messages and conversation migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180904131021) do
+ActiveRecord::Schema.define(version: 20180904132715) do
 
   create_table "apartments", force: :cascade do |t|
     t.string "name"
@@ -41,13 +41,22 @@ ActiveRecord::Schema.define(version: 20180904131021) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
-  create_table "messages", force: :cascade do |t|
-    t.boolean "read"
-    t.text "text"
-    t.integer "booking_id"
+  create_table "conversations", force: :cascade do |t|
+    t.integer "user1_id"
+    t.integer "user2_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["booking_id"], name: "index_messages_on_booking_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "conversation_id"
+    t.integer "user_id"
+    t.boolean "read", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
