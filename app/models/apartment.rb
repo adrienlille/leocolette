e262ac61af9d61fb79ea101b16ef237d214e5cd7
Adrien du Peloux <adrien.dupeloux@yahoo.fr>
2018-09-11 21:21:28 +0200

    improved show trip

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -7,4 +7,15 @@ class Apartment < ApplicationRecord
   def own?
     self.user == user
   end
+
+  def score
+    self.reviews.average(:score).round
+  end
+
+  def stars
+    stars = ''
+    stars << '<li><i class="fa fa-star" aria-hidden="true"></i></li>' * self.score
+    stars << '<li><i class="fa fa-star-o" aria-hidden="true"></i></li>' * (5 - self.score)
+    return stars
+  end
 end
